9f8f60bc9822f47baf9f105f298770ee39521be8
harshpandey002 <coding.harshp@gmail.com>
2021-11-19 20:11:56 +0530

    Mobile Nav Bar

--- components/Menu.js
+++ components/Menu.js
@@ -3,13 +3,15 @@ import styles from "@/styles/Menu.module.css";
 export default function Menu({ isOpen }) {
   return (
     <div className={`${styles.container} ${isOpen && `${styles.active}`} `}>
-      <ul className={styles.nav_links}>
-        <li>Featured Works</li>
-        <li>Projects</li>
-        <li>Tech Stack</li>
-        <li>Experience</li>
-        <li>Resume</li>
-      </ul>
+      <div className={styles.content}>
+        <ul className={styles.nav_links}>
+          <li>Featured Works</li>
+          <li>Projects</li>
+          <li>Tech Stack</li>
+          <li>Experience</li>
+          <li>Resume</li>
+        </ul>
+      </div>
     </div>
   );
 }
