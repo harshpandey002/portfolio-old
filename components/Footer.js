d515e0baafb89501d58aa77076c274ee5b784eb2
harshpandey002 <coding.harshp@gmail.com>
2021-11-20 17:33:02 +0530

    Footer Cursor Animation

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,28 @@
+import { useContext } from "react";
 import styles from "@/styles/Footer.module.css";
 import { AiFillInstagram, AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+import ThemeContext from "@/context/ThemeContext";
 
 export default function Footer() {
+  const { setCursor } = useContext(ThemeContext);
+
+  const mouseOver = (n) => {
+    const style = {
+      width: "35px",
+      height: "35px",
+      backgroundColor: "white",
+      mixBlendMode: "difference",
+    };
+
+    setCursor({ style });
+  };
+
+  const mouseOut = () => {
+    const style = {};
+    setCursor({ style });
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.left}>
@@ -10,7 +30,11 @@ export default function Footer() {
           <a id="contact">Harsh Pandey</a>
         </h1>
       </div>
-      <div className={styles.right}>
+      <div
+        onMouseOver={mouseOver}
+        onMouseOut={mouseOut}
+        className={styles.right}
+      >
         <div className={styles.icons}>
           <AiFillGithub
             onClick={() =>
